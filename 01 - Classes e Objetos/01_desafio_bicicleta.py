5518a6fdc832e7b0a17b65f0ab30f3abe743a8ac
Marcos Aleixo <maknytz@gmail.com>
2025-11-17 11:07:56 -0300

    Criando uma classe com seus atributos e métodos

---
+++ 01 - Classes e Objetos/01_desafio_bicicleta.py
@@ -0,0 +1,21 @@
+class Bicicleta:
+    def __init__(self, cor, marca, ano, valor):
+        self.cor = cor
+        self.marca = marca
+        self.ano = ano
+        self.valor = valor
+
+    def buzinar(self):
+        print("Plim plim...")
+
+    def parar(self):
+        print("Parando bicicleta...")
+        print("Bicicleta Parada!")
+
+    def correr(self):
+        print("Vrummmmm....")
+
+b1 = Bicicleta("preta", "caloi", 2022, 600)
+b1.buzinar()
+b1.correr()
+b1.parar()
